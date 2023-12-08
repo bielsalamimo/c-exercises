b6d37255db28d26153625851b285de71fde571e4
Biel Sala <biel.sala@autonoma.cat>
2023-12-08 22:00:44 +0100

    indentation

--- src/lower.c
+++ src/lower.c
@@ -2,14 +2,14 @@
 
 char lower(char c)
 {
-  return (c >= 'A' && c <= 'Z') ? (c - 'A' + 'a') : c;
+    return (c >= 'A' && c <= 'Z') ? (c - 'A' + 'a') : c;
 }
 
 int main(void)
 {
-  for (int i = 'A'; i <= 'z'; i++) {
-    printf("%c", lower(i));
-    printf("%c", (i != 'z') ? ' ' : '\n');
-  }
-  return 0;
+    for (int i = 'A'; i <= 'z'; i++) {
+        printf("%c", lower(i));
+        printf("%c", (i != 'z') ? ' ' : '\n');
+    }
+    return 0;
 }
