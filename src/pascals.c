15b20eb20bf5f5cfa6ddda569602a91e09b282d0
Biel Sala <biel.sala@autonoma.cat>
2023-12-31 14:56:15 +0100

    Test approach

--- src/pascals.c
+++ src/pascals.c
@@ -1,50 +1,33 @@
-/* NOTE: This is a horrible approach. Pls fix */
+/* NOTE: No idea how the hell I should do this */
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define UNUSED(a) 		\
-	do { 			\
-		(void) (a);	\
-	} while(0)
-
-typedef struct {
-	size_t rows_count;
-	unsigned long int *data_array;
-	size_t data_size;
-} PascalTriangle;
+#define UNUSED(a)   \
+    do {            \
+        (void) (a); \
+    } while(0)
 
 int main(int argc, char **argv)
 {
-	UNUSED(argc);
-	UNUSED(argv);
+    UNUSED(argc);
+    UNUSED(argv);
 
-	PascalTriangle p = {0};
-	p.rows_count = 10;
+    unsigned long int pascal[100];
+    pascal[0] = 1;
 
-	for (int i = 1; i <= p.rows_count; i++) {
-		p.data_size += i;
-	}
+    pascal[1] = 1;
+    pascal[2] = 1;
 
-	p.data_array = malloc(sizeof(unsigned long int) * 1000);
-	memset(p.data_array, 0, sizeof(unsigned long int) * 1000);
-	
-	// Find index of data_array that should be 1s
-	for (int i = 1; i <= p.data_size; i++) {
-		p.data_array[(i*i-i)/2] = 1;
-		if (i != 1) {
-			p.data_array[(i*i+i)/2 - 1] = 1;
-		}
-	}
+    pascal[3] = 1;
+    pascal[4] = pascal[1] + pascal[2];
+    pascal[5] = 1;
 
-	for (int row = 1; row <= p.rows_count; row++) {
-		printf("ROW %d\n", row);
-		for (int i = 0; i < row; i++) {
-			printf("%ld", *p.data_array);
-			p.data_array++;
-		}
-		printf("\n");
-	}
+    pascal[6] = 1;
+    pascal[7] = pascal[3] + pascal[4];
+    pascal[8] = pascal[4] + pascal[5];
+    pascal[9] = 1;
 
-	return 0;
+    for (int i = 0; i <= 9; i++) {
+        printf("%ld\n", pascal[i]);
+    }
+    return 0;
 }
