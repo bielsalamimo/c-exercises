2d94afbdfb3a29373f6bf7de40a7df863a9cb67f
Biel Sala <bielsalamimo@gmail.com>
2023-12-31 21:14:54 +0100

    detab.c

--- src/detab.c
+++ src/detab.c
@@ -2,7 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
-void detab(char *to, char *from, int tabstop)
+void detab(char *to, char *from, size_t tabstop);
+
+#define TABSTOP 8
+
+int main(int argc, char **argv)
+{
+    if (argc != 3) {
+        fprintf(stderr, "[ERROR] Please provide arguments\n");
+        exit(1);
+    }
+    char *filename = (char *) malloc(sizeof(*filename) * FILENAME_MAX);
+    filename = argv[1];
+    char *filename_out = (char *) malloc(sizeof(*filename_out) * FILENAME_MAX);
+    filename_out = argv[2];
+
+    size_t file_size = 0;
+    FILE *file_ptr;
+    file_ptr = fopen(filename, "r");
+    if (!file_ptr)  {
+        fprintf(stderr, "[ERROR] File '%s' not found\n", filename);
+        exit(1);
+    }
+    fseek(file_ptr, 0, SEEK_END);
+    file_size = ftell(file_ptr);
+    rewind(file_ptr);
+
+    char *file_content = (char *) malloc(sizeof(*file_content) * file_size);
+    fread(file_content, 1, file_size, file_ptr);
+    fclose(file_ptr);
+
+    char detabbed[file_size];
+    detab(detabbed, file_content, TABSTOP);
+    printf("%d\n", argc);
+    FILE *file_out_ptr = fopen(filename_out, "w");
+    if (!file_out_ptr) {
+        fprintf(stderr, "[ERROR] File '%s' not found\n", filename_out);
+        exit(1);
+    }
+    fprintf(file_out_ptr, "%s", detabbed);
+
+    return 0;
+}
+
+// TODO: Make this shit deal with newlines itself,
+// this garbage only works when _from_ points to a single line,
+// if its more than a line, it fucks up the tabstops.
+void detab(char *to, char *from, size_t tabstop)
 {
     int next_tabstop = tabstop;
     size_t from_size = strlen(from);
@@ -31,39 +77,4 @@ void detab(char *to, char *from, int tabstop)
     }
 
     to[to_i] = '\0';
-}
-
-#define MAX_SIZE 4096
-#define TABSTOP 8
-
-int main(int argc, char **argv)
-{
-    if (argc <= 1) {
-        fprintf(stderr, "[ERROR] Please provide input file\n");
-        exit(1);
-    }
-    char *filename = (char *) malloc(sizeof(*filename) * MAX_SIZE);
-    filename = argv[1];
-    char detabbed[MAX_SIZE] = {0};
-
-    char *file_content = (char *) malloc(sizeof(*file_content) * MAX_SIZE);
-    size_t file_size = 0;
-    FILE *file_ptr;
-    file_ptr = fopen(filename, "r");
-    if (!file_ptr)  {
-        fprintf(stderr, "[ERROR] File '%s' not found\n", filename);
-        exit(1);
-    }
-    fseek(file_ptr, 0, SEEK_END);
-    file_size = ftell(file_ptr);
-    rewind(file_ptr);
-    file_content = (char *) malloc(sizeof(*file_content) * file_size);
-    fread(file_content, 1, file_size, file_ptr);
-    fclose(file_ptr);
-
-    detab(detabbed, file_content, TABSTOP);
-
-    printf("%s", detabbed);
-
-    return 0;
 }
